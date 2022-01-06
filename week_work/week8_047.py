149abace030ac140f2670144e925f686c1d66d79
x06lan <x065p4mp6@gmail.com>
2022-01-06 23:51:25 +0800

    re w 47

--- week_work/week8_047.py
+++ week_work/week8_047.py
@@ -1,35 +1,35 @@
 def edf(command,content):
-    c=command.split()
+    c=command
     ct=c[0]
     
     if "ADD_W_AFTER" in ct:
-        i=int(c[1])
-        n=int(c[2])
-        w=c[4]
+        i=int(c[1])-1
+        n=int(c[2])-1
+        w=c[3]
         content[i].insert(n+1,w)
         pass
     elif"ADD_W_FRONT"in ct:
-        i=int(c[1])
-        n=int(c[2])
-        w=c[4]
+        i=int(c[1])-1
+        n=int(c[2])-1
+        w=c[3]
         content[i].insert(n,w)
         pass
     elif "ADD_S_AFTER"in ct:
-        i=int(c[1])
+        i=int(c[1])-1
         w=c[2]
-        content.insert(i+1,w)
+        content[i].insert(1,w)
         pass
     elif "ADD_S_FRONT" in ct:
-        i=int(c[1])
+        i=int(c[1])-1
         w=c[2]
-        content.insert(i,w)
+        content[i].insert(0,w)
         pass
     elif "INSERT_AFTER" in ct:
         key=c[1]
         w=c[2]
         save=[]
         for i in range(len(content)):
-            for j in i:
+            for j in range(len(content[i])):
                 if content[i][j]==key:
                     content[i].insert(j+1,w)
     elif "INSERT_FRONT" in ct:
@@ -37,10 +37,28 @@ def edf(command,content):
         w=c[2]
         save=[]
         for i in range(len(content)):
-            for j in i:
+            for j in range(len(content[i])-1,-1,-1):
                 if content[i][j]==key:
                     content[i].insert(j,w)
-    
+    elif "DEL_w" in ct:
+        i=int(c[1])-1
+        j=int(c[2])-1
+        del content[i][j]
+    elif "DEL_L" in ct:
+        i=int(c[1])-1
+        del content[i]
+    elif "REPLACE" in ct:
+        key=c[1]
+        w=c[2]
+        save=[]
+        for i in range(len(content)):
+            for j in range(len(content[i])):
+                if content[i][j]==key:
+                    content[i][j]=w
+    elif "COUNT" in ct:
+        total=0
+        for i in content:
+            total+=len(i)
     return content
 
 
@@ -49,7 +67,16 @@ x=int(x)
 y=int(y)
 text=[]
 for i in range(x):
-    text.append(input.split())
+    text.append(input().split())
 for i in range(y):
-    text=edf(input(),text)
-print()
+    command=input().split()
+    # print(text)
+    text=edf(command,text)
+    if "COUNT" in command[0]:
+        total=0
+        for i in text:
+            total+=len(i)
+        print(total)
+for i in text:
+    print(" ".join(i))
+
