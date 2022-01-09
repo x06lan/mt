001526d192feacf1a3bb4c1359bb93b71c808483
x06lan <x065p4mp6@gmail.com>
2022-01-10 05:10:14 +0800

    add mt.py (OX game)

---
+++ et.py
@@ -0,0 +1,61 @@
+def find_line(data,side):
+    out=[]
+    path=[]
+    for i in range(len(data)):
+        tem=data[i]
+        out.append(tem.count(side))
+    for i in range(len(data)):
+        tem=[data[0][i],data[1][i],data[2][i]]
+        out.append(tem.count(side))
+    tem=[data[0][0],data[1][1],data[2][2]]
+    out.append(tem.count(side))
+    tem=[data[0][2],data[1][1],data[2][0]]
+    out.append(tem.count(side))
+    return out
+def find_s(data,side,emeny):
+    init=[]
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j]!= emeny and data[i][j]!= side:
+                newdata=copy.deepcopy(data)
+                newdata[i][j]=side
+                if 3 in  find_line(newdata,side):
+                    return [True,i,j]
+                elif j!=2 and i!=2:
+                    init=[False,i,j]
+    return init
+import copy
+data=[[0]*3 for i in range(3)]
+#data.append([int(i)for i in input().split()])
+#data.append([int(i)for i in input().split()])
+#data.append([int(i)for i in input().split()])
+ix=input()
+bot=2
+human=1
+while ix!="":
+    i=int(ix[0])
+    j=int(ix[1])
+    data[i][j]=human
+    if 3 in  find_line(data,human):
+        print("you win")
+        break
+    tem1=find_s(data,bot,human)
+    tem2=find_s(data,human,bot)
+    if len(tem1)==0:
+        print("tie")
+        break
+    if data[1][1]==0:
+        data[1][1]=bot
+    elif tem1[0]:
+        data[tem1[1]][tem1[2]]=bot
+    elif tem2[0]:
+        data[tem2[1]][tem2[2]]=bot
+    else:
+        data[tem1[1]][tem1[2]]=bot
+    if 3 in find_line(data,bot):
+        print("you lose")
+        break
+    print(data[0])
+    print(data[1])
+    print(data[2])
+    ix=input()
--- week_work/week8_043.py
+++ week_work/week8_043.py
@@ -1,12 +1,4 @@
-
-x=int(input())
-save={}
-point={}
-for i in range(x):
-    tem=[j for j in input().split()]
-    save[tem[0]]=[]
-    save[tem[0]]=tem[1:len(tem)]
-    point[tem[0]]=0
+# find school 
 def deal(data,intype):
     tem=[i for i in intype.split(" ")]
     # print(data,tem)
@@ -18,21 +10,30 @@ def deal(data,intype):
                 tem2=tem2[1:len(tem2)]
             have=tem2 in data
             if "!" == i[0]:
-                out=out and not have  
+                out=out and not have
             else :
                 out=out and have
             if out==False:
                 break
     # print(out)
-    return 1 if out else 0 
-x=[i for i in input().split("+")]
-# print(x)
-for i in save:
-    for j in x:
-        point[i]+=deal(save[i],j)
-    if point[i]==0:
-        del point[i]
-count=0
-point=dict(sorted(point.items(), key=lambda item: item[1],reverse=True))
-d=" ".join([",".join([str(i),str(point[i])]) for i in point])
-print(d)
+    return 1 if out else 0
+if __name__=="__main__":
+    x=int(input())
+    save={}
+    point={}
+    for i in range(x):
+        tem=[j for j in input().split()]
+        save[tem[0]]=[]
+        save[tem[0]]=tem[1:len(tem)]
+        point[tem[0]]=0
+    x=[i for i in input().split("+")]
+    # print(x)
+    for i in save:
+        for j in x:
+            point[i]+=deal(save[i],j)
+        if point[i]==0:
+            del point[i]
+    count=0
+    point=dict(sorted(point.items(), key=lambda item: item[1],reverse=True))
+    d=" ".join([",".join([str(i),str(point[i])]) for i in point])
+    print(d)
