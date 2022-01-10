f9804ae87982349f383d3cc117c8ea5acf9c946c
x06lan <x065p4mp6@gmail.com>
2022-01-11 02:53:27 +0800

    finsh 50 not 48

---
+++ week_work/week8_048.py
@@ -0,0 +1,60 @@
+def find_line(data,side):
+    out=[]
+    path=[]
+    for i in range(len(data)):
+        tem=data[i]
+        out.append(tem.count(side))
+    for i in range(len(data)):
+        tem=[data[0][i],data[1][i],data[2][i]]
+        out.append(tem.count(side))
+    tem=[data[0][0],data[1][1],data[2][2]]
+    out.append(tem.count(side))
+    tem=[data[0][2],data[1][1],data[2][0]]
+    out.append(tem.count(side))
+    return out
+    return init
+def print_table(data):
+    print("\n".join([" ".join([str(j) for j in i]) for i in data]))
+def deal_input(value):
+    i=value//3
+    j=value%3
+    return i,j
+data=[[0]*3 for i in range(3)]
+who_frist=int(input())-1
+step=[int(i)-1 for i in input().split()]
+human=1
+bot=2
+side_change=[[human,bot],[bot,human]]
+out=[]
+have_error=False
+
+for i in range(len(step)):
+    value=step[i]
+    tem=i%2
+    #print(i,value)
+    i1,j1=deal_input(value)
+    if  data[i1][j1]!=0:
+        print("Error")
+        have_error=True
+    else:
+        data[i1][j1]=side_change[who_frist][tem]
+        if 3 in find_line(data,side_change[who_frist][tem]):
+            if side_change[who_frist][tem]==2:
+                if not have_error:print("OK")
+                print_table(data)
+                print("Computer win")
+                break
+            else:
+                if not have_error:print("OK")
+                print_table(data)
+                print("Player win")
+                break
+    if i==len(step)-1:
+        if 0 not in  data[0]and 0 not in  data[1] and 0 not in data[2]:
+            print_table(data)
+            print("Tie")
+
+        else:
+            print_table(data)
+            print("Undecided")
+
---
+++ week_work/week8_050.py
@@ -0,0 +1,54 @@
+import copy
+save=[]
+roomn,meets=input().split()
+meets=int(meets)
+roomn=int(roomn)
+maxnumber_arr=[]
+for i in range(roomn):
+    maxnumber_arr.append(int(input().split()[1]))
+for i in range(meets):
+    index,number,start,end=input().split()
+    start=int(start)
+    number=int(number)
+    end=int(end)
+    save.append([start,end,number])
+def col(data,targe,maxnumber):
+    if targe[2]>maxnumber:
+        return True
+    for i in data:
+        if len(i)==0:
+            pass
+        elif targe[0]>=i[0] and targe[0]>=i[1]:
+            pass
+        elif i[0]>=targe[0] and i[0] >=targe[1]:
+            pass
+        else:
+            return True
+    return False
+def check(rooms,meets ):
+    if len(meets)==0:
+        total=0
+        for i in rooms:
+            for j in i:
+                total+=j[1]-j[0]
+        #if total>20:
+        #    print(rooms,total)
+        return [total]
+    out=[]
+    newmeet=copy.deepcopy(meets)
+    meet=newmeet.pop(0)
+    for i in range(len(rooms)):
+        temrooms=copy.deepcopy(rooms)
+        ck=not col(temrooms[i],meet,maxnumber_arr[i])
+        #print(maxnumber_arr[i],temrooms[i],meet)
+        #print(ck)
+        if ck:
+            temrooms[i]+=[meet]
+            out+=check(temrooms,newmeet)
+    out+=check(rooms,newmeet)
+    return out
+
+emty_room=[[]for i in range(roomn)]
+gm=check(emty_room,save)
+#print(gm)
+print(max(gm))
