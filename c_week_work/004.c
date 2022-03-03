8ff1f01009274c892090bec8f528331ea5457eb0
x06lan <x065p4mp6@gmail.com>
2022-03-04 01:14:59 +0800

    week2

---
+++ c_week_work/004.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+int main() {
+	float r=0;
+	scanf("%f",&r);
+	printf("%.3f\n",r*r*PI);
+	printf("%.3f\n",2*r*PI);
+	
+	return 0;
+}
---
+++ c_week_work/005.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int i=0,j=0;
+	int name[3];
+	int class[6][10];
+	for(i=0 ;i<6;i++){
+		for(j=0;j<10;j++){
+			class[i][j]=4;
+		}
+	}
+	for (i=0;i<3;i++){
+		int class_n=0;
+		scanf("%d",&name[i]);
+		scanf("%d",&class_n);
+		for(j=0;j<class_n;j++){
+			int time=0;
+			scanf("%d",&time);
+			int tem=0;
+			tem=floor(time/10);
+			if (class[tem][time%10]!=4){
+				printf("%d\n",class[tem][time%10]);
+				printf("%d and %d conflict on %d\n",name[class[tem][time%10]],name[i],time);
+
+			}
+			else{
+				class[tem][time%10]=i;
+			}
+		}
+	}
+	return 0;
+}
