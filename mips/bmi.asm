e283caa1431c2bd8308609f954952184021a8277
x06lan <x065p4mp6@gmail.com>
2022-03-09 03:10:24 +0800

    bmi finshs

--- mips/bmi.asm
+++ mips/bmi.asm
@@ -4,16 +4,31 @@
     input_bmi: .word 80
     prompt_h:     .asciiz "high: "
     prompt_w:     .asciiz "weigh: "
+	prompt_bmi:		.asciiz "bmi:"
     output:     .asciiz "echo: "
 
 .text
-    la $a0, prompt_h
-    li $v0, 4
-    syscall
+	input_h_d:
+		la $a0, prompt_h
+		li $v0, 4
+		syscall
 
-    li $v0, 6 #set value 
-    syscall
+		li $v0, 6
+		syscall
 
-	mov.s $f12, $f0
-	li $v0,2
-	syscall
+		mov.s $f2, $f0
+	input_w_d:
+		la $a0, prompt_w
+		li $v0, 4
+		syscall
+
+		li $v0, 6
+		syscall
+
+		mov.s $f3, $f0
+	output_bmi:
+		mul.s $f4 $f2 $f2
+		div.s $f5 $f3 $f4
+		mov.s $f12 $f5
+		li $v0 ,2
+		syscall
