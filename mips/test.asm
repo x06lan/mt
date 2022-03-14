98b2f9ab5e59c3a466cf0c6452e86696758d9283
x06lan <x065p4mp6@gmail.com>
2022-03-14 19:27:13 +0800

    mips for loop

--- mips/test.asm
+++ mips/test.asm
@@ -1,11 +1,23 @@
-
+.data 
+	newline:.asciiz "\n"
 .text
-.globl main
-main: 
 
-li    $v0, 11
-syscall
-la    $a0, 'a'
-syscall
-li $v0, 10
-syscall
+
+main:
+    li $t0,0
+    li $t1,0
+    li $t3,0
+loop:
+    li $v0,1
+	addi  $a0,$t0,0
+	syscall
+    li $v0,4
+	la $a0,newline
+	syscall
+
+    addi $t0,$t0,1
+    bgt $t0,14,exit
+	
+    j loop
+exit:
+    li $v0,0
