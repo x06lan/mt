eacf968cb157225bec5f6e72bf8ec7dbbac646c4
x06lan <x065p4mp6@gmail.com>
2022-03-19 01:12:48 +0800

    fix

--- c_week_work/012.c
+++ c_week_work/012.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdint.h>
 #include <string.h>
-int command(int a,int b){
+int get_gcd(int a,int b){
 	int tem=0;
 	while(a>=1 &&b>=1){
 		tem=a%b;
@@ -18,7 +18,6 @@ void deal(int a,int b,int aa,int bb,char sign[]){
 		return;
 	}
 	int out[3]={0};
-	// printf("%d %d\n",aa,bb);
 
 	if(strcmp(sign,"/")==0){
 		int temp=aa;
@@ -26,7 +25,6 @@ void deal(int a,int b,int aa,int bb,char sign[]){
 		bb=temp;
 		sign="*";
 	}
-	// printf("%d %d\n",aa,bb);
 	a=(b>0)?a:(-a);
 	b=(b>0)?b:(-b);
 	aa=(bb>0)?aa:(-aa);
@@ -46,18 +44,23 @@ void deal(int a,int b,int aa,int bb,char sign[]){
 		out[1]=b*bb;
 	}
 	if (abs(out[0])/abs(out[1])>=1){
+		int gcd=get_gcd(abs(out[0]),out[1]);
 		out[2]=out[0]/out[1];
 		out[0]=out[0]%out[1];
-		int gcd=command(abs(out[0]),out[1]);
-		printf("%d(%d/%d)\n",out[2],abs(out[0])/gcd,out[1]/gcd);
-	}else if(out[0]==0){
-		printf("0\n");
-	}else if(out[1]==1){
-		printf("%d\n",out[0]);
-	}
-	else{
-		int gcd=command(abs(out[0]),out[1]);
-		printf("%d/%d\n",out[0]/gcd,out[1]/gcd);
+		out[0]=abs(out[0])/gcd;
+		out[1]=out[1]/gcd;
+		if(out[0]==0)printf("%d\n",out[2]);
+		else printf("%d(%d/%d)\n",out[2],out[0],out[1]);
+	}else {
+		if(out[0]==0){
+			printf("0\n");
+		}else if(out[1]==1){
+			printf("%d\n",out[0]);
+		}
+		else{
+			int gcd=get_gcd(abs(out[0]),out[1]);
+			printf("%d/%d\n",out[0]/gcd,out[1]/gcd);
+		}
 	}
 }
 int main() {
