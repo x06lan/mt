7608fa94266d094af65f79cc09cd771488bd0408
x06lan <x065p4mp6@gmail.com>
2022-03-30 00:47:09 +0800

    write big div

--- c_week_work/new015.c
+++ c_week_work/new015.c
@@ -42,8 +42,57 @@ int compare(int *a,int *b){
 	}
 	return 0;
 }
-
-
+void big_mul(int *a,int *b,int *c){
+	int i,j,up;
+	for(i=0;i<=45;i++){
+		for(j=0;j<=45;j++){
+			int tem=a[i]*b[j]+up+c[i+j];
+			up=tem/10;
+			c[i+j]=tem%10;
+		}
+	}
+}
+void big_add(int *a,int *b,int *c){
+	int i,up;
+	for(i=0;i<=45;i++){
+		int tem=a[i]+b[i]+up;
+		up=tem/10;
+		c[i]=tem%10;
+	}
+}
+void big_sub(int *a,int *b,int *c){
+	int i,up;
+	for(i=0;i<=45;i++){
+		int tem=a[i]-b[i]-up;
+		if(tem<0){
+			tem+=10;
+			up=1;
+		}
+		else{
+			up=0;
+		}
+		c[i]=tem;
+	}
+}
+void big_div(int *a,int *b,int *c){
+	int i,j,up;
+	for(i=0;i<=45;i++){
+		for(j=9;j>=0;j--){
+			int tem[90];
+			int tem2[90];
+			str_int((char)j,tem2);
+			big_mul(b,tem2,tem);
+			if(compare(a,tem)>=0){
+				big_sub(a,tem,a);
+				c[i]=j;
+				break;
+			}
+			else if(j==0){
+				c[i]=0;
+			}
+		}
+	}
+}
 int main() {
 	char a[45],b[45];
 	int x[90],y[90],z[90];
