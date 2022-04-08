88c1f16a0b3a83099f08f827b56b186826f20464
x06lan <x065p4mp6@gmail.com>
2022-04-09 02:32:05 +0800

    finsh fb and 019

--- c_week_work/019.c
+++ c_week_work/019.c
@@ -2,16 +2,47 @@
 #include <stdlib.h>
 #include <math.h>
 
+void rec(int *a,int x, int y, int *bin,int size,int *count,char *str){
+	char nowtext=str[*count];
+	int i,j;
+	int sign=nowtext-'0';
+	//printf("%d %d %d %d\n",x,y,*bin,sign);
+	if(sign==2){
+		(*bin)/=2;
+		*count=*count+1;
+		rec(a,x,y,bin,size,count,str);
+		*count=*count+1;
+		rec(a,x+*bin,y,bin,size,count,str);
+		*count=*count+1;
+		rec(a,x,y+*bin,bin,size,count,str);
+		*count=*count+1;
+		rec(a,x+*bin,y+*bin,bin,size,count,str);
+		(*bin)*=2;
+		
+	}
+	else if(sign==1){
+		//printf("+%d %d %d %d\n",x,y,*bin,sign);
+		for(i=y;i<y+*bin;i++){
+			for(j=x;j<x+*bin;j++){
+				*(a+i*size+j)=sign;
+			}
+		}
+	}
+}
 int main() {
 	int i,j;
 	int size=0;
-	char instr[100];
+	char instr[1000];
 	for(i=0;i<100;i++)instr[i]='-';
 	scanf("%s",&instr);
 	scanf("%d",&size);
+	int bin=size;
 	int *df=malloc(sizeof(int)*size*size);
-	for(i=0;i<size;i++)for(j=0;j<size;j++)*(df+i+j)=9;
-	//recode(size,df);
-	pri(df,size);
-	
+	for(i=0;i<size*size;i++)*(df+i)=0;
+	i=0;
+	rec(df,0,0,&bin,size,&i,instr);
+	for(i=0;i<size*size;i++)if(*(df+i)==1)printf("%d,%d\n",i/size,i%size);
+	bin=0;
+	for(i=0;i<size*size;i++)if(*(df+i)==1)bin=1;
+	if(bin==0)printf("all white");
 }
--- mips/fb.asm
+++ mips/fb.asm
@@ -1,6 +1,6 @@
 .data
 	msg1: .asciiz "Give a number: "
-	list : .space 400 #400 byte for 100 int 
+	list: .space 400
 
 .text
 .globl main
@@ -13,12 +13,18 @@ main:
     add $a0, $v0, $zero # move to $a0
 
 	
-	la $t0 ,list
-	sw 1,0($t0)
-	sw 1,4($t0)
+	addi $s1,$a0,0
+	la $t0,list
+	li $t6,1
+	sw $t6,0($t0)
+	sw $t6,4($t0)
+
     jal fb
 
-
+	addi $a0,$a0,-1
+	sll $a0,$a0,2
+	add $t0,$t0,$a0
+	lw $a0,($t0)
     li $v0, 1
     syscall
     li $v0, 10
@@ -27,21 +33,27 @@ fb:
 	addi $sp,$sp,-8
 	sw $ra,0($sp)
 	sw $a0,4($sp)
-	beq $a0,-2,fbout
-
-	add $t0,$t0,$a0
+	beq $a0,0,fbout
 	addi $a0,$a0,-1
 	jal fb
 fbout:
 	lw $ra,0($sp)
-	lw $t3,4($sp)
+	lw $t2,4($sp)
 	addi $sp,$sp,8
-
-	lw $t2 $t3($t0)
-	addi $t3 $t3 4
-	lw $t4 $t3($t0)
-	add $t5 $t2 $t4
-	addi $t3 $t3 4
-	sw $t5 $t3(t0)
 	
+	addi $a0,$t2,0 # keep $a0 as input
+
+	addi $t1,$t0,0
+	sll $t2,$t2,2
+
+	add $t1,$t1,$t2 #a[i]
+	lw $t3,0($t1)
+
+	addi $t1,$t1,4 #a[i+1]
+	lw $t4,0($t1)
+
+	add $t5,$t3,$t4 #a[i+2]=a[i]+a[i+1]
+	addi $t1,$t1,4
+	sw $t5,($t1)
+
 	jr $ra
