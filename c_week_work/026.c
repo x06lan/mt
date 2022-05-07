4b77d79a71e44a3288ca774ef397c996bd805951
x06lan <x065p4mp6@gmail.com>
2022-05-07 20:44:53 +0800

    fix c26

--- c_week_work/026.c
+++ c_week_work/026.c
@@ -5,24 +5,26 @@
 double f1(double x,double a){
 	double tem=a-x*x;
 	tem=pow(tem,0.5L);
-	return tem;
+	return fabs(tem);
 
 }
 double f2(double x,double a){
 	double tem=(a*x*x*x+7.0L*x)/pow(a+x,0.5L);
-	return tem;
+	return fabs(tem);
 
 }
 double (*farr[2])(double,double)={f1,f2};
 double t(double (*fun)(double ,double),double a,double p,double q,int n){
-	double out=0;
+	double out=0.0;
 	double up=(q-p)/n;
 	double d=2.0;
 	int i;
-	for(i=0;i<n;i++){
-		out+=fabs(fun(p+i*up,a)+fun(p+(i+1L)*up,a))*up/d;
+	for(i=0;i<n+1;i++){
+		double tem=fun(p+i*up,a);
+		if(i!=0&&i!=n)tem*=2.0L;
+		out+=tem;
 	}
-	return out;
+	return out/2.0L*up;
 }
 int input(){
 	int intype=0;
@@ -32,12 +34,12 @@ int input(){
 	scanf("%d",&intype);
 	if(intype==0)return 0;
 	scanf("%lf%lf%lf%lf",&a,&p,&q,&err);
-	double last=0;
-	double now=0;
+	double last=0.0;
+	double now=0.0;
 	while(1){
 		now=t(farr[intype-1],a,p,q,n);
 		if(n!=1){
-			if(fabs(last-now)<pow(0.1L,err)){
+			if(fabs(now-last)<pow(0.1,err)){
 				printf("%.12lf\n",now);
 				break;
 			}
