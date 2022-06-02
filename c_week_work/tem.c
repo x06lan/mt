88fb933799fb0aeab1866a9f7cf1a73f03a16b91
x06lan <x065p4mp6@gmail.com>
2022-06-03 04:19:58 +0800

    invert

--- c_week_work/tem.c
+++ c_week_work/tem.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-void f(int *a, int*b,int*c){
-	int *temp=a;
-	*b=(*c)*(*temp);
-	*c=*temp;
-	*a=10;
-	a=b;
-	b=c;
-	c=temp;
-}
 int main() {
-	int *num=malloc(sizeof(int)*10);
-	*(num+1)=5;
-	free(num);
-	int a,*b,**c;
-	a=5;
-	b=&a;
-	c=&b;
-	printf("%d\n",**c);
-	printf("%d\n",(**c)++);
-	printf("%d\n",**c);
+	int w,h;
+	scanf("%d %d",&w,&h);
+	char  file[h][w];
+	int i,j;
+	for(i=0;i<h;i++){
+		scanf("%s",file[i]);
+	}
+
+	printf("\n");
+	for(i=h-1;i>=0;i--){
+		for(j=w-1;j>=0;j--){
+			printf("%c",file[i][j]);
+		}
+		printf("\n");
+	}
 }
