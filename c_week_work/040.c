2ad5ce4cdf409612cf078c21fd807c4a8058b3b6
x06lan <x065p4mp6@gmail.com>
2022-06-13 13:15:07 +0800

    fix funtion

--- c_week_work/040.c
+++ c_week_work/040.c
@@ -7,7 +7,6 @@ typedef struct node_t{
 	struct node_t *l;
 	struct node_t *f;
 }node;
-
 node *newnode(char v){
 	node *tem=malloc(sizeof(node));
 	tem->r=NULL;
@@ -44,39 +43,50 @@ char *match(char *a,char *b ){
 	for(i=0;a[i]!='\0';i++){
 		for(j=0;b[j]!='\0';j++){
 			if(a[i]==b[j]){
-				max=max>j?max:j;
-				if(max=-1)min=j;
+				
+				if(max==-1)min=j;
 				else min=min<j?min:j;
+				max=max>j?max:j;
+				
 			}
 		}
 	}
-	char ans[100]="";
+	char *ans=malloc(sizeof(char)*100);
 	for (int i = 0; i < max-min; i++){
-		ans[i+min]=a[i];
+		ans[i]=b[i+min];
 	}
 	ans[max]='\0';
 	return ans;
 }
-node *findpre(char *mid,char *pre){
+node *findpre(char *mid,char *pre,int type){
 	if(mid=='\0'||pre=='\0'){
 		return NULL;
 	}
-	node *tem=newnode(*pre);
+	node *tem;
+	if(type==0)tem=newnode(*pre);
+	else tem=newnode(pre[strlen(pre)-1]);
+
 	char l[100]="",r[100]="";
 	find(mid,*pre,l,r);
-	
-
+	char *lpre=match(l,pre);
+	char *rpre=match(r,pre);
+	tem->l=findpre(l,lpre,type);
+	tem->r=findpre(r,rpre,type);
 	return tem;
 }
 
 
-
-
 int main(){
+	
+//BCAEDGHFI
+//ABCDEFGHI
+
 	char order=' ';
 	scanf("%c",&order);
-	char *path="";
-	scanf("%c",path);
+	char path[100]="EDGHFI";
+	char path2[100]="ABCDEFGHI";
+	printf("%s\n",match(path,path2));
+	//scanf("%c",path);
 
 	return 0;
 }
