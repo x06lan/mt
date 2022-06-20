4dfc3f0de3724342a182f8e26fb3bf7f1e4a30e2
x06lan <x065p4mp6@gmail.com>
2022-06-20 15:58:51 +0800

    finsh bin free sort

--- c_week_work/sort_bintree.c
+++ c_week_work/sort_bintree.c
@@ -13,7 +13,11 @@ node *newnode(int v){
 	tem->r=NULL;
 	return tem;
 }
-void pr(node *root){
+int pr(node *root){
+	if(root==NULL){
+		printf("NULL");
+		return 0;
+	}
 	if(root->l!=NULL)pr(root->l);
 	if(root)printf("%d ",root->v);
 	if(root->r!=NULL)pr(root->r);
@@ -21,16 +25,32 @@ void pr(node *root){
 void append(node **root ,int v){
 	if((*root)==NULL){
 		(*root)=newnode(v);
-	}else if (v<(*root)->v){
-		if((*root)->r!=NULL)append((*root)->r,v);
+	}else if (v>(*root)->v){
+		if((*root)->r!=NULL)append(&(*root)->r,v);
 		else (*root)->r=newnode(v);
 	}
 	else{
-		if((*root)->l!=NULL)append((*root)->l,v);
+		if((*root)->l!=NULL)append(&(*root)->l,v);
 		else (*root)->l=newnode(v);
 	}
 }
 
 int main() {
+	int v;
+	char c=' ';
+	node *root=NULL;
+	scanf("%c",&c);
+	while(c!='e'){
+		if(c=='p'){
+			pr(root);
+			printf("\n");
+		}
+		else if(c=='i'){
+			scanf("%d",&v);
+			append(&root,v);
+		}
+		scanf("%c",&c);
+	}
 
+	return 0;
 }
