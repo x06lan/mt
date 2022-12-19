9ea4c913d6cebdf2f02a0a60615392b944d10dcd
x06lan <x065p4mp6@gmail.com>
2022-12-19 23:12:49 +0800

    linear algebra hw2 done

--- misc/martix/hw2.cpp
+++ misc/martix/hw2.cpp
@@ -12,11 +12,61 @@ int main() {
   // a
   // printf("%lf\n", (A[0]).mat[0][0]);
   // Matrix<1, 2> ea = A.eigenValue();
-  A.print();
-  printf("%lf\n", (A - A.eye() * A.eigenValue().mat[0][1]).det());
+  // A.print();
+  printf("A:\n");
+  printf("zero vector cant be eigenVector by definetion\n");
+  printf("eigenValue λ1 is\n");
+  printf("[%.4lf ]\n", A.eigenValue().mat[0][0]);
+  // A.eigenValue()[0].print();
+  printf("eigenVector for λ1 is\n");
+  A.eigenVector()[0].print();
+  // A.eigenVector().print();
   printf("\n");
-  A.eigenValue().print();
+
+  printf("B:\n");
+  printf("eigenValue λ1,λ2 is\n");
+  B.eigenValue().print();
+  printf("eigenVector for λ1,λ2 is\n");
+  B.eigenVector().print();
   printf("\n");
-  A.eigenVector().print();
+
+  printf("C:\n");
+  printf("eigenValue λ1,λ2 is\n");
+  C.eigenValue().print();
+  printf("eigenVector for λ1,λ2 is\n");
+  C.eigenVector().print();
+  printf("\n");
+
+  printf("V:\n");
+  printf("eigenValue λ1,λ2 is\n");
+  V.eigenValue().print();
+  printf("eigenVector for λ1,λ2 is\n");
+  V.eigenVector().print();
+  printf("\n");
+
+  printf("W:\n");
+  printf("λ1==λ2\n");
+  printf("eigenValue λ1 is\n");
+  printf("[%.4lf ]\n", W.eigenValue().mat[0][0]);
+  printf("eigenVector for λ1 is\n");
+  W.eigenVector()[0].print();
+  printf("\n");
+
+  printf("X:\n");
+  printf("eigenValue λ1,λ2 is\n");
+  X.eigenValue().print();
+  printf("eigenVector for λ1,λ2 is\n");
+  B.eigenVector().print();
+  printf("\n");
+
+  printf("Z:\n");
+  printf(
+      "zero vector cant be eigenVector by definetion\nso not have eigenValue "
+      "and eigenVector");
+  // printf("eigenValue is\n");
+  // Z.eigenValue().print();
+  // printf("eigenVector is\n");
+  // Z.eigenVector()[0].print();
+  printf("\n");
   return 0;
 }
